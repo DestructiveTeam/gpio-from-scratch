1c010232414830a976f6f2d264d86019fea836e0
Nguyen Dinh Phi <phind.uet@gmail.com>
2018-06-29 18:01:08 +0800

    DIY

--- gpio_char.c
+++ gpio_char.c
@@ -9,7 +9,11 @@
 #define GPIO_COUNT 1
 #define DRIVER_NAME "ar71xx_onio"
 #define CLASS_NAME "oni_gpio"
+#define OAR71XX_APB_BASE 0X18000000
+#define OAR71XX_GPIO_BASE OAR71XX_APB_BASE+0x00040000
+#define OAR71XX_RES_SIZE 0x100
 
+static __iomem* gpio_base;
 static struct cdev ar71xx_gpio_chardev;
 dev_t device_number;
 class device_class;
@@ -49,6 +53,9 @@ static int __init ar71xx_gpio_char_init(void) {
     cdev_init(&ar71xx_gpio_chardev, &ar71xx_gpio_fops);
     cdev_add(&ar71xx_gpio_chardev, device_number, GPIO_COUNT);
 
+    //Step 4: ioremap
+    gpio_base = ioremap(OAR71XX_GPIO_BASE, OAR71XX_RES_SIZE);
+
     return 0; 
 } 
  
@@ -58,6 +65,30 @@ static void __exit ar71xx_gpio_char_exit(void) {
     class_destroy(&device_class);
     unregister_chrdev_region(device_number, GPIO_COUNT);
 } 
+
+int gpio_open(struct inode *inode, struct file *filp){
+    //Currently,there is nothing to do
+    return 0;
+}
+
+int gpio_release(struct inode *inode, struct file *filp){
+    //Currently,there is nothing to do
+    return 0;
+}
+
+/*
+*   Get data from user and write it to device.
+*/
+ssize_t gpio_write(struct file *filp, const char __user *buf, size_t count, loff_t *pos){
+
+}
+
+/*
+*   Read data from device and transfer it to user space.
+*/
+ssize_t gpio_read(struct file *filp, char __user *buf, size_t count, loff_t *pos){
+
+}
  
 module_init(ar71xx_gpio_char_init); 
 module_exit(ar71xx_gpio_char_init); 
